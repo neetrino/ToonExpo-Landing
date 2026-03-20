5506b310e33ba4236b16ebc6ef61b26d3a26f875
AKhanjyan <armen.khanjyan200327@mail.ru>
2026-03-21 00:48:54 +0400

    mobile

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,31 @@ import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
 const ADMIN_LOGIN = "/admin/login";
+const MOBILE_UA_REGEX =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i;
+
+function isMobileProjectPath(pathname: string): boolean {
+  return /^\/p\/[^/]+$/.test(pathname);
+}
+
+function isMobileRequest(req: Parameters<Parameters<typeof auth>[0]>[0]): boolean {
+  const chMobile = req.headers.get("sec-ch-ua-mobile");
+  if (chMobile === "?1") {
+    return true;
+  }
+
+  const userAgent = req.headers.get("user-agent") ?? "";
+  return MOBILE_UA_REGEX.test(userAgent);
+}
 
 export default auth((req) => {
   const { pathname } = req.nextUrl;
+  if (isMobileProjectPath(pathname) && isMobileRequest(req)) {
+    const url = req.nextUrl.clone();
+    url.pathname = `${pathname}/mobile`;
+    return NextResponse.redirect(url);
+  }
+
   if (!pathname.startsWith("/admin")) {
     return NextResponse.next();
   }
@@ -20,5 +42,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/admin", "/admin/:path*"],
+  matcher: ["/admin", "/admin/:path*", "/p/:path*"],
 };
